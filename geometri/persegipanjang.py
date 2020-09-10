d67c6ef2ecba6f2b7ab065c1fd63cb639cf08edb
RydhaValenda <49052032+rydhavalenda@users.noreply.github.com>
2020-09-10 19:52:13 +0700

    Membuat fungsi persegi panjang menggunakan OOP

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,3 @@
+class PersegiPanjang():
+    def info(self):
+        return 'Modul menghitung rumus-rumus persegi panjang'
---
+++ main.py
@@ -0,0 +1,5 @@
+from geometri.persegipanjang import PersegiPanjang
+
+print('Menggunakan OOP')
+p1 = PersegiPanjang()
+print(p1.info())
